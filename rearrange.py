6c37a66bf890e889ca4bd8e14fc4c2af05deca14
yeshrajawat <yeshrajawat@gmail.com>
2020-07-11 07:18:23 +0530

    Python Script is added to rearrange.py file to rearange the given input which will be a name

--- rearrange.py
+++ rearrange.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+
+import re
+
+def rearrange(name):
+ pattern = r"([\w .]*),(\w .]*)"
+ result = re.search(pattern,name)
+ if result != None:
+  return ("{},{}".format(result[2],result[1]))
+rearrange("Yesh,Rajawat")
 
